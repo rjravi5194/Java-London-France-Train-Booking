497ca3c9ad188e50db14929f562f4f4ba55abef5
rjravi5194 <116703744+rjravi5194@users.noreply.github.com>
2024-05-17 00:27:56 +0530

    Add files via upload

---
+++ booking/src/main/java/com/train/london/france/booking/service/interfaces/BookingService.java
@@ -0,0 +1,18 @@
+package com.train.london.france.booking.service.interfaces;
+
+import com.train.london.france.booking.model.BookingDetailsDto;
+import com.train.london.france.booking.model.CustomerDetailsDao;
+import com.train.london.france.booking.model.SectionDetailsDto;
+import com.train.london.france.booking.model.TicketDetailsDao;
+
+import java.util.List;
+
+public interface BookingService {
+    BookingDetailsDto bookTicket(TicketDetailsDao ticketDetailsDao);
+
+    List<SectionDetailsDto> ViewTrainSectionDetails(String sectionType, String journeyDate);
+
+    String removeUserFromTrainSection(BookingDetailsDto bookingDetailsDto);
+
+    BookingDetailsDto modifyUserTicket(CustomerDetailsDao customerDetailsDao, String journeyDate,int seatNumber);
+}
